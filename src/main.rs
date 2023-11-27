56d22af533d0249f4be8b2fb252024be9c5f5586
Sasha Lišková <sliskova@proton.me>
2023-11-27 09:08:15 -0500

    add watch

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,108 @@
-use maud::{html, Markup};
-use orgize::{Element, Org}
+use std::error::Error;
+use std::path::Path;
+
+use maud::{html, Markup, DOCTYPE, PreEscaped};
+use orgize::Org;
 use url::Url;
+use std::fs;
+use std::io;
+
+mod watch;
 
 pub struct Head<'a> {
 	pub title: &'a str,
 	pub description: &'a str,
 	pub url: Url,
-	pub css_hash: &'a str,
+}
+
+impl Default for Head<'_> {
+	fn default() -> Head<'static> {
+		Head {
+			title: "A title",
+			description: "A description",
+			url: Url::parse("https://a.url").unwrap(),
+		}
+	}
 }
 
 pub fn render(meta: Head, content: Org) -> Result<Markup, Box<dyn Error>> {
 	let mut writer = Vec::new();
-	Org::parse(content).write_html(&mut writer)?;
+	content.write_html(&mut writer)?;
 	let html_content = String::from_utf8(writer)?;
-	html! {
+	Ok(html! {
 		(DOCTYPE)
 		html lang="en" {
 			head {
 				title { (meta.title) }
 				meta charset="utf-8";
-				link rel="stylesheet" href="/style.css";
+				link rel="stylesheet" href="style.css";
 			}
 			body {
 				header {
 					h1 { (meta.title) }
 				}
-				main #main { (html_content) }
+				(PreEscaped(html_content))
 			}
 		}
-	}
+	})
 }
 
 fn build() -> Result<(), Box<dyn Error>> {
 	fs::remove_dir_all("dist").ok();
 	fs::create_dir_all("dist")?;
 
+	// if exists
+	copy_dir("static", "dist")?;
+
+	// let layout =
+
+	build_page("content/index.org", "dist/index.html")?;
+	build_page("content/cube-mnemonics.org", "dist/cube-mnemonics.html")?;
+
+	Ok(())
+}
+
+fn build_page<F, T>(from: F, to: T) -> Result<(), Box<dyn Error>>
+where
+	F: AsRef<Path> + Send + Sync,
+	T: AsRef<Path> + Send,
+{
+	let org_content = fs::read_to_string(from)?;
+	let markup = Org::parse(&org_content);
+
+	fs::write(
+		to,
+		render(Head::default(), markup)?.into_string(),
+	)?;
+
+	Ok(())
+}
+
+fn copy_dir<F, T>(from: F, to: T) -> io::Result<()>
+where
+	F: AsRef<Path> + Send + Sync,
+	T: AsRef<Path> + Send,
+{
+	fs::read_dir(&from)?
+		.map_while(|item| item.ok())
+		.try_for_each(|entry| {
+			let filename = entry.file_name();
+			let old_path = entry.path();
+
+			let new_path = to.as_ref().join(filename);
+			if new_path.exists() {
+				return Err(io::Error::other("asdf"))
+			}
+
+			if old_path.is_dir() {
+				fs::create_dir(&new_path)?;
+				copy_dir(old_path, &new_path)
+			} else {
+				fs::copy(old_path, new_path).map(|_| ())
+			}
+		})
 }
 
 fn main() {
-    println!("Hello, world!");
+	let _ = watch::watch();
 }
---
+++ src/watch.rs
@@ -0,0 +1,135 @@
+use notify_debouncer_mini::{new_debouncer, DebounceEventResult, notify::RecursiveMode};
+
+use std::error::Error;
+use std::io;
+use std::net::{TcpListener, TcpStream};
+use std::path::Path;
+use std::process::{Command, Stdio};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+pub fn watch() -> Result<(), Box<dyn Error>> {
+    // cf. https://doc.rust-lang.org/std/net/struct.TcpListener.html
+    // Start a TCP server. Let the system assign a port.
+    // This returns an io::Result, which should tell you what sort of
+    // things could possibly go wrong.
+    let websocket_server = TcpListener::bind("localhost:0")?;
+
+    // Figure out what port we're on.
+    let websocket_port = websocket_server.local_addr()?.port();
+
+    // crate::build(Some(websocket_port))?;
+    crate::build()?;
+
+    // https://docs.rs/crossbeam-channel/latest/crossbeam_channel/
+    // TODO Try to replace this with stdlib std::sync::mpsc for fun!
+    // Obtain a transmitter and receiver for `notify`.
+    // TODO Turbofish???
+    let (notify_tx, notify_rx) = crossbeam_channel::unbounded::<DebounceEventResult>();
+
+    // https://docs.rs/notify-debouncer-mini/latest/notify_debouncer_mini/
+    // This one can't be replaced, it goes with `notify`.
+    let mut debouncer = new_debouncer(Duration::from_millis(500), notify_tx)?;
+
+    let (build_tx, build_rx) = crossbeam_channel::unbounded::<()>();
+
+    // https://doc.rust-lang.org/std/thread/fn.spawn.html
+    // TODO move??
+    let build_thread = thread::spawn(move || {
+        notify_rx.iter()
+                 .filter_map(|rx| {
+                     match rx {
+                         Ok(_) => Some(()),
+                         Err(e) => { eprintln!("Error: {:?}", e); None },
+                     }
+                 })
+                 .map(|_| {
+                     // Run the command `cargo run build --websocket-port PORT`.
+                     Command::new("cargo")
+                         .arg("run")
+                         .arg("build")
+                         .args(["--websocket-port", &websocket_port.to_string()])
+                         .stdout(Stdio::inherit())
+                         .stderr(Stdio::inherit())
+                         .spawn()
+                         .expect("cargo failed to start")
+                         .wait()
+                         .expect("cargo error")
+                 })
+                 // Log nonzero exit statuses.
+                 .try_for_each(|exit_status| {
+                        if exit_status.success() {
+                            build_tx.send(())
+                        } else {
+                            eprintln!("Error: Got status {:?}", exit_status);
+                            Ok(())
+                        }
+                 })
+
+
+        });
+
+    // https://docs.rs/tungstenite/latest/tungstenite/
+    // TODO What is all this doing???
+    let sockets: Arc<Mutex<Vec<tungstenite::WebSocket<TcpStream>>>> = Arc::new(Mutex::new(Vec::new()));
+    let sockets_clone = sockets.clone();
+
+    let websocket_thread = thread::spawn(move || {
+        websocket_server.incoming().for_each(|stream| {
+            // TODO UNWRAPS
+            let websocket = tungstenite::accept(stream.unwrap()).unwrap();
+            sockets.lock().unwrap().push(websocket);
+        });
+    });
+
+    let reload_thread = thread::spawn(move || {
+        build_rx.iter()
+                .for_each(|_| {
+                    let mut sockets = sockets_clone.lock().unwrap();
+
+                    // TODO Make this stateless
+                    let mut broken = vec![];
+
+                    sockets.iter_mut()
+                        .enumerate()
+                        .for_each(|(index, socket)| {
+                            socket.send("reload".into())
+                                .err()
+                                .map(|err| {
+                                    match err {
+                                        tungstenite::error::Error::Io(e) => {
+                                            if e.kind() == io::ErrorKind::BrokenPipe {
+                                                broken.push(index);
+                                            }
+                                        },
+                                        e => { eprintln!("Error: {:?}", e); }
+                                    };
+                                });
+                        });
+
+                    broken.into_iter().rev().for_each(|index| {
+                        sockets.remove(index);
+                    });
+                })
+
+    });
+
+    debouncer.watcher().watch(Path::new("./content"), RecursiveMode::Recursive)?;
+
+    let dist = std::env::current_dir()?.join("dist");
+    let server = file_serve::ServerBuilder::new(dist)
+        .hostname("0.0.0.0")
+        .port(1729)
+        .build();
+
+    println!("Running on http://{}", server.addr());
+    println!("Hit Ctrl-C to stop");
+    server.serve()?;
+
+    build_thread.join().unwrap()?;
+    reload_thread.join().unwrap();
+    websocket_thread.join().unwrap();
+
+    Ok(())
+}
